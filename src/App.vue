deb30b877b4b99a995cbfd7c9d86d524919e715e
EthanSMorse <emorse@brooklynsteamcenter.org>
2023-05-15 14:55:13 -0400

    Dun stuff.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-  import {ref} from 'vue'
+  import {ref, watch} from 'vue'
   let input = ref ("")
-  let todos = ref ([])
+  let todos = ref (JSON.parse(window.localStorage.getItem('todos')))
 
+  watch(todos, function(value) {
+    window.localStorage.setItem('todos', JSON.stringify(value))
+  }, {deep: true})    
 
   function addToDo () {
     if (input.value == "") {
@@ -23,14 +26,14 @@
   <h1>My ToDo Application</h1>
 
   <input @keydown.enter ="addToDo" v-model="input">
-  <br>
-  <br>
-  <button @click="addToDo">Add ToDo</button>
+  <button id="AddTD-style" @click="addToDo">Add ToDo</button>
 
-  <div class="li-style" v-for="(todo, index) in todos">
-    <button @click= "deleteToDo(index)">Delete</button>
-    <input type="checkbox" v-model="todo.complete">
-    {{ todo.text }}
+  <div class="li-box">
+    <div class="li-style" v-for="(todo, index) in todos">
+      <button @click= "deleteToDo(index)">Delete</button>
+      <input class="CB-style" type="checkbox" v-model="todo.complete">
+      {{ todo.text }}
+    </div>
   </div>
 </template>
 
@@ -42,10 +45,32 @@
     border-style: solid;
     border-width: 2px;
     border-radius: 10px;
-    border-color: forestgreen;
+  }
+
+  .CB-style {
+    
+  }
+
+  #AddTD-style {
+    margin-left: 20px;
+    border-color: gold;
   }
 
   .li-style {
     list-style: none;
+    font-size: larger;
+    margin: 10px;
+  }
+
+  .li-box {
+    background-color: forestgreen;
+    height: fit-content;
+    padding: 10px;
+    min-width: 11px;
+    border-style: solid;
+    border-width: 2px;
+    border-radius: 10px;
+    border-color: gold;
+    margin-top: 35px;
   }
 </style>
